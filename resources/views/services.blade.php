6ee99169d6e020165ad82c530863ac7930bfc7f9
Neville <otiendeneville@gamil.com>
2026-05-07 09:58:53 +0300

    addition of button

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -4,5 +4,11 @@
 
 
     <x-layout title="Services">
-        <h1>Services Page</h1>
+        
+        <div>
+            <h1>Services Page</h1>
+            <p>This is the Services Page</p>
+            <button style="background: orangered";>Click Me!</button>
+
+        </div>
     </x-layout>
