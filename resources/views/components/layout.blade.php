287ab6913879b14d33b157db5907928711b8e3d6
Neville <otiendeneville@gamil.com>
2026-05-07 09:26:13 +0300

    addition services page

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,6 +13,7 @@
     <nav>
         <a href="/">Home</a>
         <a href="/about">About Us</a>
+        <a href="/services">Services</a>
         <a href="/contact">Contact Us</a>
     </nav>
     <main>
---
+++ resources/views/services.blade.php
@@ -0,0 +1,8 @@
+
+
+
+
+
+    <x-layout title="Services">
+        <h1>Services Page</h1>
+    </x-layout>
