8d3cd1d18d4b1b7522194e3996b95a5a7b782cf8
Neville <otiendeneville@gamil.com>
2026-05-11 12:16:48 +0300

    icrating ideas

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
   $ideas = DB::table('idea')->get();
+  
   //  $ideas = session()->get('ideas', []);
   // $dd = ($ideas);
 dd($ideas);
@@ -13,8 +14,14 @@ dd($ideas);
 });
 Route::post('/ideas',function(){
   $idea = request('idea');
-  session()->push('ideas' ,$idea);
-  return redirect('/');
+  // session()->push('ideas' ,$idea);
+  // return redirect('/');
+   DB::table('idea')->insert([
+        'content' => $idea, // Make sure your column name matches!
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]);
+
 });
 
 
