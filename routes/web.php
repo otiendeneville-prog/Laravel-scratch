f3e6cc6cce49a64ab2312b13ef7653a72be47277
Neville <otiendeneville@gamil.com>
2026-05-13 12:20:14 +0300

    finding idea in first column

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,19 @@
 use App\Models\Idea;
 use Illuminate\Support\Facades\Route;
 
-// 1. Root URL Fallback (Prevents 404 if you visit just http://127.0.0.1:8000)
 Route::get('/', function () {
     return redirect('/ideas');
 });
 
-// 2. Display all ideas
 Route::get('/ideas', function () {
-    $ideas = Idea::all();
-    
+    // $ideas = Idea::all();
+    $ideas =Idea::find(1);
     return view('ideas', [
         'ideas' => $ideas,
     ]);
 });
 
-// 3. Display a single specific idea (Fixed matching $id parameter variable)
 Route::get('/ideas/{id}', function ($id) {
-    // Looks for the specific ID or safely triggers a 404 if not found
     $idea = Idea::findOrFail($id);
 
     return view('ideas', [
@@ -28,9 +24,7 @@ Route::get('/ideas/{id}', function ($id) {
     ]);
 });
 
-// 4. Handle form submission to create an idea
 Route::post('/ideas', function () {
-    // Validates incoming input field named 'ideas'
     request()->validate([
         'ideas' => 'required'
     ]);
@@ -39,6 +33,5 @@ Route::post('/ideas', function () {
         'description' => request('ideas')
     ]);
 
-    // Redirects back to the index view so the new entry displays immediately
     return redirect('/ideas');
 });
