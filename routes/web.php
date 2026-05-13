0d3be64a43a7d47441a059d5a6d7e610e752e317
Neville <otiendeneville@gamil.com>
2026-05-13 07:22:00 +0300

    finding a specific idea

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
-<?php
+ <?php
 
-// use App\idea;
 use App\Models\Idea;
 
  use Illuminate\Support\Facades\Route;
@@ -13,7 +12,7 @@ use App\Models\Idea;
         ]);
       
  });
- Route::get('/ideas/{id}', function(){
+ Route::get('/ideas/{id}', function($Id){
         $ideas = Idea::all();
 
 
@@ -28,8 +27,45 @@ use App\Models\Idea;
          'description'=> request('ideas')
        ]);
  });
+ 
 
 
 
 
+//  <?php
 
+// use App\Models\Idea;
+// use Illuminate\Support\Facades\Route;
+
+// // 1. Display all ideas
+// Route::get('/ideas', function () {
+//     $ideas = Idea::all();
+//     return view('ideas', [
+//         'ideas' => $ideas,
+//     ]);
+// });
+
+// // 2. Display a single specific idea
+// Route::get('/ideas/{id}', function ($id) {
+//     // Find the specific idea by its ID, or throw a clean 404 if it doesn't exist
+//     $idea = Idea::findOrFail($id);
+
+//     return view('idea-single', [
+//         'idea' => $idea,
+//     ]);
+// });
+
+// // 3. Handle the form submission to create an idea
+// Route::post('/ideas', function () {
+//     // Validate the incoming request data first
+//     request()->validate([
+//         'ideas' => 'required'
+//     ]);
+
+//     Idea::create([
+//         'description' => request('ideas')
+//     ]);
+
+//     // Redirect back to the list page so the user sees the new entry
+//     return redirect('/ideas');
+// });
