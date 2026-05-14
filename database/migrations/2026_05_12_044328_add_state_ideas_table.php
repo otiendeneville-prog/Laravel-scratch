8fc1afbd62ecdab3a49c5c42ec97cf20ccea3028
Neville <otiendeneville@gamil.com>
2026-05-14 09:04:15 +0300

    addition of default pending

--- database/migrations/2026_05_12_044328_add_state_ideas_table.php
+++ database/migrations/2026_05_12_044328_add_state_ideas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('ideas',function(Blueprint $table){
         $table->id();
-        $table->string('state');
+        $table->string('state')->default('pending');
         $table ->timestamps();
         $table->string('description');
         $table ->string('conlumn');
