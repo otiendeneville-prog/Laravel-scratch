83ac58e83487d8f092a4874f68e0989b9e6a3313
Neville <otiendeneville@gamil.com>
2026-05-14 07:54:33 +0300

    addition of patch

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -3,7 +3,6 @@
   
   <form method="POST" action="/ideas" >
    @csrf
-   @method('PATCH');
    <div class="col-span-full">
       <label for="idea" class="block text-sm/6 font-medium text-white">New Idea</label>
       <div class="mt-2">
@@ -11,13 +10,14 @@
       </div>
            <p class="mt- 3 text-sm/6 text-white">You have an idea want to save for later.</p>
    </div>
+    @method('PATCH')
    <div class="mt-6 flex items-center gap-x-6">
       <button type="submit" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Update</button>
    </div>
    <div class="mt-6 text-white">
       @if($ideas -> count())
       <h2 class="font-bold">Ideas in podcast!</h2>
-      @foreach($ideas as $idea)
+      @foreach($ideas as $description)
          <li class="text-sm">{{$idea->description}}</li>
       @endforeach
       @endif
