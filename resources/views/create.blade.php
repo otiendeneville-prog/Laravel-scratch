9291c0edd0fb949efbdaabe5869b4e80a1c82f01
Neville <otiendeneville@gamil.com>
2026-05-15 07:54:02 +0300

    adding create file.blade

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -18,20 +18,5 @@
           </button>
        </div>
     </form>
-
-    <!-- 2. LISTING ALL IDEAS & ACTIONS -->
-    <div class="mt-10 text-white">
-        @if($ideas->count())
-            <h2 class="font-bold text-lg mb-4">Ideas in podcast!</h2>
-            <ul class="space-y-4">
-                @foreach($ideas as $singleIdea)
-                    <li class="flex items-center justify-between p-4 bg-white/5 rounded-md">
-                        <div>
-                            <span class="text-sm block">{{ $singleIdea->description }}</span>
-                            <!-- Link to take user to the specific edit page -->
-                            <a href="/ideas/{{ $singleIdea->id }}" class="text-xs text-indigo-400 hover:underline">Edit details</a>
-                        </div>
-    </div>
-
   </div>
 </x-layout>
